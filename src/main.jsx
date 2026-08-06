57b300ca0e182dbed8a0d64cfd26ee87f971efd3
Akshut Goyal <akshutgoyal@gmail.com>
2026-08-06 11:19:23 +0530

    UI

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 import App from './App'
 import './styles.css'
 import './polish.css'
+import './main-polish.css'
+import './icon-polish.css'
 import './controls.css'
 
 createRoot(document.getElementById('root')).render(<App />)
